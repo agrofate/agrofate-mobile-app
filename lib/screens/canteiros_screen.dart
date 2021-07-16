8b66aa4d9221ffca5a3820f6ab13b34abca415cf
alexandrerussi <alexandrerussijunior@gmail.com>
2021-07-16 15:57:53 -0300

    Front: launcher_icon criado e ajustes na lista de canteiros

--- lib/screens/canteiros_screen.dart
+++ lib/screens/canteiros_screen.dart
@@ -44,7 +44,7 @@ class _CanteirosScreenState extends State<CanteirosScreen> {
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              padding: const EdgeInsets.all(30),
               child: Column(
                 children: [
                   ListView.builder(
@@ -52,87 +52,93 @@ class _CanteirosScreenState extends State<CanteirosScreen> {
                     physics: const NeverScrollableScrollPhysics(),
                     itemCount: canteiros.length,
                     itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        // decoration:
-                        //     const BoxDecoration(color: Colors.black12),
-                        child: Column(
-                          children: [
-                            const SizedBox(
-                              height: 2,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Container(
-                                  width: 65,
-                                  height: 65,
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey.withOpacity(0.1),
-                                    shape: BoxShape.rectangle,
-                                    borderRadius: BorderRadius.circular(10),
-                                    image: DecorationImage(
-                                      // todo: trocar img do canteiro de acordo com banco - se tiver
-                                      image: AssetImage(
-                                          "assets/images/canteiro_padrao.jpg"),
-                                      fit: BoxFit.cover,
+                      return GestureDetector(
+                        onTap: () {
+                          // TODO: enviar para página de detalhes do canteiro
+                          print("página do cantero");
+                        },
+                        child: Container(
+                          // decoration:
+                          //     const BoxDecoration(color: Colors.black12),
+                          child: Column(
+                            children: [
+                              const SizedBox(
+                                height: 2,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    width: 65,
+                                    height: 65,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey.withOpacity(0.1),
+                                      shape: BoxShape.circle,
+                                      // borderRadius: BorderRadius.circular(10),
+                                      image: DecorationImage(
+                                        // todo: trocar img do canteiro de acordo com banco - se tiver
+                                        image: AssetImage(
+                                            "assets/images/canteiro_padrao.jpg"),
+                                        fit: BoxFit.cover,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                const SizedBox(
-                                  width: 15,
-                                ),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      canteiros[index].nomeCanteiro,
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 15,
+                                  const SizedBox(
+                                    width: 15,
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        canteiros[index].nomeCanteiro,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 15,
+                                        ),
                                       ),
-                                    ),
-                                    const SizedBox(
-                                      height: 1,
-                                    ),
-                                    Text(
-                                      "Criação: " +
-                                          canteiros[index].dataCriacao,
-                                      style: const TextStyle(
-                                        fontSize: 14,
+                                      const SizedBox(
+                                        height: 1,
                                       ),
-                                    ),
-                                    const SizedBox(
-                                      height: 1,
-                                    ),
-                                    Text(
-                                      "Última atualização: " +
-                                          canteiros[index]
-                                              .dataUltimaAtualizacao,
-                                      style: const TextStyle(
-                                        fontSize: 14,
+                                      Text(
+                                        "Criação: " +
+                                            canteiros[index].dataCriacao,
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            const SizedBox(
-                              height: 2,
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.only(left: 80, top: 6),
-                              child: Divider(
-                                thickness: 0.8,
+                                      const SizedBox(
+                                        height: 1,
+                                      ),
+                                      Text(
+                                        "Última atualização: " +
+                                            canteiros[index]
+                                                .dataUltimaAtualizacao,
+                                        style: const TextStyle(
+                                          fontSize: 14,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
                               ),
-                            ),
-                          ],
+                              const SizedBox(
+                                height: 2,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 80, top: 6),
+                                child: Divider(
+                                  thickness: 0.8,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                       );
                     },
                   ),
                   const SizedBox(
-                    height: 10,
+                    height: 15,
                   ),
                   ButtonWidget(
                     title: 'NOVO CANTEIRO',
